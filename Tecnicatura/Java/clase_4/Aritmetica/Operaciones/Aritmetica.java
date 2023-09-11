ff712efa69106a7a393cd3467828bc084eecd18b
Aldana Ravalle <aldana.veronica.ravalle@gmail.com>
2023-09-11 17:32:37 -0300

    Clase 4 - java

---
+++ Tecnicatura/Java/clase_4/Aritmetica/Operaciones/Aritmetica.java
@@ -0,0 +1,14 @@
+package clase_4.Aritmetica.Operaciones;
+
+public class Aritmetica {
+    // Atributos de la clase
+    int a; // valor por default 0
+    int b;
+
+    // Metodo
+    public void sumarNumeros(){
+        int resultado = a + b;
+        System.out.println("Resultado: "+resultado);
+    }
+}
+
---
+++ Tecnicatura/Java/clase_4/clases/Persona.java
@@ -0,0 +1,18 @@
+package clase_4.clases;
+
+public class Persona {
+    // Atributos de la clase (Caracteristivas)
+    String nombre;
+    String apellido;
+
+    // Metodos de la clase (Acciones)
+    // public -> indica que le metodo se puede utilizar fuera de la clase
+    // void -> no regresa información
+    // () -> recibe información
+    public void obtenerInfo() {
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+
+    }
+    
+}
