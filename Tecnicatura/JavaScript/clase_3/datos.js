6fbf4bb926ca33b8c5fdd7690901651aac8049e2
Aldana Ravalle <aldana.veronica.ravalle@gmail.com>
2023-09-14 17:25:44 -0300

    Clases de JavaScript 1-4

---
+++ Tecnicatura/JavaScript/clase_3/datos.js
@@ -0,0 +1,30 @@
+// Tipos de datos en JavaScript
+
+// Tipo string
+// LAs variables se pueden reutilizar
+var nombre = 'Daniel';
+console.log(nombre);
+
+// Tipo numerico
+var numero = 2493;
+console.log(numero);
+
+// Tipo objeto
+var objeto = {
+    nombre : 'Ana',
+    apellido : 'Martinez',
+    edad: 23
+}
+console.log(objeto);
+
+// Tipo boolean
+var booleano = true;
+console.log(booleano);
+
+// Tipo funcion
+function funcion(){}
+console.log(funcion);
+
+// Tipo symbol
+var simbolo = Symbol('Mi simbolo')
+console.log(simbolo);
