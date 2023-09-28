248ee18fd0961d977c528ef08528df042058df97
Aldana Ravalle <aldana.veronica.ravalle@gmail.com>
2023-09-28 18:34:26 -0300

    Javascript - clase 6

---
+++ Aldana Ravalle - Segundo Semestre/JavaScript/clase_6/concatenacion.js
@@ -0,0 +1,27 @@
+// Concatenar con variables
+var nombre = 'Mario';
+var apellido = 'Aguilera';
+var usuario1 = nombre+' '+apellido;
+
+console.log(usuario1);
+
+// Concatenar directamente
+var usuario2 = 'Abril'+' '+'Gonzales';
+console.log(usuario2);
+
+// Concatenar distintos datos - Contexto String / Cadena
+var usuario3 = nombre + 123; // se lee como tipo string
+console.log(usuario3)
+
+usuario3 = nombre + 1 + 2 + 3;  // se lee como tipo string, no se suma
+console.log(usuario3)
+
+usuario3 = nombre + (1 + 2 + 3); // Se lee como int, se suman los valores
+console.log(usuario3)
+
+usuario3 = 1 + 2 + 3 + nombre; // Se lee como int, se suman los valores
+console.log(usuario3)
+
+// Concatenar con operadores
+nombre += apellido;
+console.log(nombre)
