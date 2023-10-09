a21b3120651deb1daf20a4f981910eb7ac1b737e
Aldana Ravalle <aldana.veronica.ravalle@gmail.com>
2023-10-09 19:32:00 -0300

    Add files via upload

---
+++ Aldana Ravalle - Segundo Semestre/JavaScript/clase_7/ejercicio1.js
@@ -0,0 +1,10 @@
+// Ejercicio: Encontrar números Pares e Impares
+
+let numero = 24; // numero ingresado directamente
+
+if (numero % 2 == 0){
+    console.log('El número es par')
+}
+else{
+    console.log('Es un número impar')
+}
