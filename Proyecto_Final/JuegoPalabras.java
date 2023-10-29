dc319c755b219709eb5ea4aaf52636ab3bc9f07f
Aldana Ravalle <aldana.veronica.ravalle@gmail.com>
2023-10-29 18:00:03 -0300

    proyecto final - 1

---
+++ Proyecto_Final/JuegoPalabras.java
@@ -0,0 +1,82 @@
+package Proyecto_Final;
+
+import java.util.Scanner;
+
+public class JuegoPalabras {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Reglas del juego
+        System.out.println();
+        System.out.println("  ______________________________________________________________________________");
+        System.out.println(" |                                                                              |");
+        System.out.println(" |                     - Bienvenido al Juego de Palabras -                      |");
+        System.out.println(" |                                                                              |");
+        System.out.println(" |   Este juego consiste en ingresar una palabra por turno.                     |");
+        System.out.println(" |   Para ganar, la primera sílaba de las palabras ingresadas debe coincidir    |");
+        System.out.println(" |   con la última sílaba de la palabra anterior.                               |");
+        System.out.println(" |   El jugador que se equivoque, pierde.                                       |");
+        System.out.println(" |______________________________________________________________________________|");
+
+        System.out.println();
+
+        // Ingresar nombres de los jugadores
+        String jugador1, jugador2;
+
+        System.out.print("Ingrese el nombre del Jugador 1: ");
+        jugador1 = scanner.nextLine();
+
+        System.out.print("Ingrese el nombre del Jugador 2: ");
+        jugador2 = scanner.nextLine();
+
+        // Definir variables
+        String palabraAnterior = "";
+        String jugadorActual = jugador1;
+        String jugadorSiguiente = jugador2;
+        boolean juegoActivo = true;
+
+        // Ciclo del juego
+        while (juegoActivo) {
+            System.out.println("\nPalabra anterior: " + palabraAnterior);
+            System.out.print(jugadorActual + ", ingresa una palabra: ");
+            String palabra = scanner.nextLine();
+
+            // Comparar silabas
+            if (palabraAnterior.isEmpty()
+                    || obtenerUltimaSilaba(palabraAnterior).equalsIgnoreCase(obtenerPrimeraSilaba(palabra))) {
+                palabraAnterior = palabra;
+                String temp = jugadorActual;
+                jugadorActual = jugadorSiguiente;
+                jugadorSiguiente = temp;
+            } else {
+                System.out.println();
+
+                System.out.println(jugadorActual + " ha perdido. La palabra no coincide.");
+                juegoActivo = false;
+            }
+        }
+        scanner.close();
+        System.out.println("¡" + jugadorSiguiente + " ha ganado el juego!");
+    }
+
+    // Funciones para obtener silabas
+    private static String obtenerUltimaSilaba(String palabra) {
+        palabra = palabra.toLowerCase();
+        int longitud = palabra.length();
+        if (longitud < 2) {
+            return palabra;
+        }
+        return palabra.substring(longitud - 2);
+    }
+
+    private static String obtenerPrimeraSilaba(String palabra) {
+        palabra = palabra.toLowerCase();
+        int longitud = palabra.length();
+        int indiceEspacio = palabra.indexOf(" ");
+        if (indiceEspacio == -1 || indiceEspacio >= longitud - 1) {
+            return palabra.substring(0, 2);
+        }
+        return palabra.substring(0, 2) + palabra.substring(indiceEspacio);
+    }
+}
---
+++ Proyecto_Final/MenuDeJuegos.java
@@ -0,0 +1,57 @@
+package Proyecto_Final;
+
+import java.util.Scanner;
+
+public class MenuDeJuegos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("");
+        System.out.println("Bienvenido al Menú de Juegos");
+        System.out.println("1. Juego de Palabras");
+        System.out.println("2. Otro juego");
+        System.out.println("3. Otro juego");
+        System.out.println("4. Otro juego");
+        System.out.println("5. Otro juego");
+        System.out.println("6. Otro juego");
+        System.out.println("7. Otro juego");
+        System.out.println("8. Otro juego");
+        System.out.println("");
+
+        System.out.print("Elige un juego (1/8): ");
+
+        int opcion = scanner.nextInt();
+        scanner.nextLine();
+
+        if (opcion == 1) {
+            System.out.println("");
+            JuegoPalabras.main(args);
+
+        } else if (opcion == 2) {
+            // otro juego
+        
+        } else if (opcion == 3) {
+            // otro juego
+        
+        } else if (opcion == 4) {
+            // otro juego
+        
+        } else if (opcion == 5) {
+            // otro juego
+        
+        } else if (opcion == 6) {
+            // otro juego
+        
+        } else if (opcion == 7) {
+            // otro juego
+        
+        } else if (opcion == 8) {
+            // otro juego
+
+        } else {
+            System.out.println("Opción no válida. Saliendo del Menú de Juegos.");
+        }
+
+        scanner.close();
+    }
+}
