2a85309c5becd5a6aad0000359662d53f9cdd6a2
Simon Carabajal <135768121+SimonCarabajal@users.noreply.github.com>
2023-10-29 19:33:22 -0300

    Add files via upload

---
+++ Simon Carabajal- Segundo Semestre Tecnicatura/Java/clase_8/Encapsulamiento/test/PersonaPrueba.java
@@ -0,0 +1,33 @@
+package clase_8.Encapsulamiento.test;
+
+import clase_8.Encapsulamiento.dominio.*;
+
+public class PersonaPrueba {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Manuel", 50000,false);
+
+        // Modificar a traves de los metodos
+        persona1.setNombre("Maria");
+        System.out.println("El nombre de persona1 es: "+persona1.getNombre());
+        System.out.println("El sueldo de persona1 es: "+persona1.getSueldo());
+        System.out.println("La valor booleano de persona1 es: "+persona1.isEliminado());
+
+        // persona1.nombre = "Maria"; -> ya no se puede usar, error en el acceso
+        // System.out.println("El nombre de persona 1 es: "+persona1.nombre);
+
+        // Tarea, segundo objeto
+        Persona persona2 = new Persona("Ismael", 40500,true);
+
+        // Modificar
+        persona2.setNombre("Ramona");
+        persona2.setSueldo(30000);
+        persona2.setEliminado(false);
+
+        // Mostrar
+        System.out.println("El nombre de persona1 es: "+persona2.getNombre());
+        System.out.println("El sueldo de persona1 es: "+persona2.getSueldo());
+        System.out.println("La valor booleano de persona1 es: "+persona2.isEliminado());
+
+        
+    }
+}
