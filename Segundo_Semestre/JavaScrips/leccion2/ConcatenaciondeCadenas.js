922a7fd7053515a44b6148ff66d527b1b3a74c5f
Gianella A. Achetoni <135769239+Gianella-A-Achetoni@users.noreply.github.com>
2023-11-02 08:59:33 -0300

    Clase10 JavaScrip

--- Segundo_Semestre/JavaScrips/leccion2/ConcatenaciondeCadenas.js
+++ Segundo_Semestre/JavaScrips/leccion2/ConcatenaciondeCadenas.js
@@ -7,7 +7,7 @@ console.log(nombreCompleto);
 var nombreCompleto2 = "Mercedes" + " " + "Rosales";
 console.log(nombreCompleto2);
 
-var juntos = nombre + 2005;
+var juntos = nombre + 2005; // no se recomienda el uso de var
 console.log(juntos);
 
 juntos = nombre + ( 1 + 8); //Al poner parentecis respeta la suma
@@ -23,3 +23,9 @@ let x,y;
 x = 17, y = 21;
 let z = x + y;
 console.log(z)
+
+let nombre2 = "Juan";
+console.log(nombre2)
+
+const apellido2 = "Martiniano";//Las contantes no pueden ser reasignadas
+console.log(apellido2)
