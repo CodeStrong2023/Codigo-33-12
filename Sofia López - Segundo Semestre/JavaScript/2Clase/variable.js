7a5d7a1893ef8541759d3aef69b77be433e54c32
Sofía López <135775058+Soofbb@users.noreply.github.com>
2023-11-08 16:12:07 -0300

    JavaScript

---
+++ Sofia López - Segundo Semestre/JavaScript/2Clase/variable.js
@@ -0,0 +1,4 @@
+var nombre = " Sofía";
+var apellido = " López";
+var mensaje = "dice, Hola Mundo!!!";
+console.log(nombre + apellido + mensaje)
---
+++ Sofia López - Segundo Semestre/JavaScript/7Clase/ejercicio_1.js
@@ -0,0 +1,10 @@
+//Ejercicio: Encontrar números Pares e Impares
+
+let numero = 24; //Numero ingresado manualmente
+
+if (numero % 2 == 0){
+    console.log('El número es par')
+}
+else{
+    console.log('Es un número impar')
+}
