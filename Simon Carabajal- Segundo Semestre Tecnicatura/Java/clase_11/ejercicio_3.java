7593d08252090209c429eff7033e9b6d06b2569d
Simon Carabajal <135768121+SimonCarabajal@users.noreply.github.com>
2023-11-09 20:02:00 -0300

    Add files via upload

---
+++ Simon Carabajal- Segundo Semestre Tecnicatura/Java/clase_11/ejercicio_3.java
@@ -0,0 +1,57 @@
+package clase_11;
+
+import java.util.Scanner;
+
+public class ejercicio_3 {
+    /*
+     * Ejercicio 3: Leer 5 números por teclado, almacenarlos en
+     * un arreglo y a continuación realizar la media de los
+     * números positivos, la media de los negativos y contar el
+     * número de ceros.
+     */
+
+    public static void main(String[] args) {
+
+        Scanner datos = new Scanner(System.in);
+        int numeros[] = new int[5];
+        int negativos = 0, positivos = 0, mediaNegativos, mediaPositivos;
+        int conteoCeros = 0, conteoNegativos = 0, conteoPositivos = 0;
+
+        System.out.println("\nIngresar los numeros");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Ingrese un número:  ");
+            numeros[i] = datos.nextInt();
+            if (numeros[i] > 0) {
+                positivos += numeros[i];
+                conteoPositivos++;
+            } else if (numeros[i] < 0) {
+                negativos += numeros[i];
+                conteoNegativos++;
+            } else {
+                conteoCeros++;
+            }
+        }
+
+        System.out.println("\n - Numeros positivos - ");
+        if (conteoPositivos == 0) {
+            System.out.println("No hay numeros positivos");
+        } else {
+            mediaPositivos = positivos / conteoPositivos;
+            System.out.println("La media de los numeros positivos es: " + mediaPositivos);
+        }
+
+        System.out.println("\n - Numeros negativos - ");
+        if (conteoNegativos == 0) {
+            System.out.println("No hay numeros negativos");
+        } else {
+            mediaNegativos = negativos / conteoNegativos;
+            System.out.println("La media de los numeros negativos es: " + mediaNegativos);
+        }
+
+        System.out.println("\n - Ceros - ");
+        System.out.println("La cantidad de ceros es: " + conteoCeros);
+
+        datos.close();
+    }
+}
---
+++ Simon Carabajal- Segundo Semestre Tecnicatura/Java/clase_12/Matrices.java
@@ -0,0 +1,78 @@
+package clase_12;
+
+import clase_11.arreglos.domain.Persona;
+
+public class Matrices {
+    public static void main(String[] args) {
+
+        // Iniciallizar la matriz
+        int edades[][] = new int[3][2];
+        System.out.println("Edades: " + edades);
+
+        // Llenar la matriz manualmente
+        edades[0][0] = 5;
+        edades[0][1] = 10;
+
+        edades[1][0] = 15;
+        edades[1][1] = 20;
+
+        edades[2][0] = 25;
+        edades[2][1] = 30;
+
+        // Mostrar la matriz
+        System.out.println("\nMatriz manual");
+        System.out.println("Edades 0 - 0: " + edades[0][0]);
+        System.out.println("Edades 0 - 1: " + edades[0][1]);
+        System.out.println("Edades 1 - 0: " + edades[1][0]);
+        System.out.println("Edades 1 - 1: " + edades[1][1]);
+        System.out.println("Edades 2 - 0: " + edades[2][0]);
+        System.out.println("Edades 2 - 1: " + edades[2][1]);
+
+        System.out.println("\nMatriz iterada");
+        for (int fila = 0; fila < edades.length; fila++) {
+            for (int columna = 0; columna < edades[fila].length; columna++) {
+                System.out.println("edades " + fila + " - " + columna + ": " + edades[fila][columna]);
+            }
+        }
+
+        // Sintaxis simplificada
+        System.out.println("\nMatriz frutas");
+        String frutas[][] = { { "Manzana Roja", "Manzana Verde" }, { "Banana", "Platano" },
+                { "Naranja", "Mandarina" } };
+        imprimir(frutas);
+
+        /*
+         * for (int fila = 0; fila < frutas.length; fila++) {
+         * for (int columna = 0; columna < frutas[fila].length; columna++) {
+         * System.out.println("frutas " + fila + " - " + columna + ": " +
+         * frutas[fila][columna]);
+         * }
+         * }
+         */
+
+        System.out.println("\nMatriz personas");
+
+        // Creamos una matriz de objetos
+        Persona personas[][] = new Persona[2][3];
+
+        // Asignamos valores a la matriz
+        personas[0][0] = new Persona("Marcelo");
+        personas[0][1] = new Persona("Mariela");
+        personas[0][2] = new Persona("Aldana");
+
+        personas[1][0] = new Persona("Abril");
+        personas[1][1] = new Persona("Lautaro");
+        personas[1][2] = new Persona("Cleo");
+
+        imprimir(personas);
+    }
+
+    // Metodo imprimir
+    public static void imprimir(Object matriz[][]) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.println("matriz " + i + " - " + j + ": " + matriz[i][j]);
+            }
+        }
+    }
+}
