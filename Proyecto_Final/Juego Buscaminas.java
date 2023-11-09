43bea205633f1274c8a601ffa589fff7a3ddd6c3
Simon Carabajal <135768121+SimonCarabajal@users.noreply.github.com>
2023-11-09 20:08:35 -0300

    Update Juego Buscaminas.java

--- Proyecto_Final/Juego Buscaminas.java
+++ Proyecto_Final/Juego Buscaminas.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
-
+package Proyecto_Final;
 public class JuegoBuscaminas {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
