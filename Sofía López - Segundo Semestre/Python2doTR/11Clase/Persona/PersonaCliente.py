59af610e603cb6bbeb350af382d4a20cda104106
Sofía López <sofiablopezz@hotmail.com>
2023-11-10 15:16:02 -0300

    Clase 11 Python

---
+++ Sofía López - Segundo Semestre/Python2doTR/11Clase/Persona/PersonaCliente.py
@@ -0,0 +1,7 @@
+from Persona import*
+
+persona1 = Persona("Anastasia", 47)
+print(persona1)
+
+empleado3 = Empleado('Kimberly', 45, 30000)
+print(empleado3)
---
+++ Sofía López - Segundo Semestre/Python2doTR/11Clase/Vehiculo/vehiculo.py
@@ -0,0 +1,53 @@
+class Vehiculo:
+    
+    '''
+    Definir una clase padre Llamada Vehiculo y dos clases hijas lLamadas
+    Auto y Bicicleta, las cuales heredan de la clase padre Vehicule. 
+    La clase padre debe tener los siguientes atributos y métodos:
+    
+    Vehicule (clase padre)
+        Atributos(color, ruedas)
+        Métodos (__init__(color, ruedas) y __str__())
+
+    Auto(clase hija de Vehiculo)
+        Atributos(velocidad (km/hr))
+        Métodos (__init_(color, ruedas, velocidad) y __str__())
+
+    Bicicleta(clase hija de Vehicule)
+        Atributos (tipe(urbana/montaña/etc.)
+        Métodos (__init__(color, ruedas, tipe) y _str_()
+        
+    Crear un objete de cada clase
+    '''
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+
+    def __str__(self):
+        return 'Color: ' + self.color + 'Ruedas: ' + str(self.ruedas)
+
+class Auto(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super(). __init__(color, ruedas)
+        self.velocidad = velocidad
+            
+    def __str__(self):
+        return super().__str__()+', Velocidad(km/hr): ' + self.velocidad
+
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo = tipo
+    
+    def __str__(self):
+        return super().__str__()+', Tipo: '+self.tipo
+
+vehiculo = Vehiculo('rojo', 4)
+print(vehiculo)
+        
+auto = Vehiculo('verde', 4, 120)
+print(auto)
+
+bicicleta = Bicicleta('amarillo', 2, 'Urbana')
+print(bicicleta)
