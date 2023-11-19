c114652cca4e3f5cc2432efc72fdd70eabc13d92
ArriolaBelen <137359966+ArriolaBelen@users.noreply.github.com>
2023-11-19 05:14:36 -0300

    Add files via upload

---
+++ STecnicatura2023/Java/Leccion7/Paquetes/src/test/TestUtileria.java
@@ -0,0 +1,16 @@
+
+package test;
+//import ar.com.codesystem.*;
+import ar.com.codesystem.Utileria;
+
+//import static ar.com.codesystem.Utileria.imprimir;//Sólo aplica para métodos estáticos
+
+
+
+public class TestUtileria {
+     public static void main(String[] args) {
+        Utileria.imprimir("Saludos a todos los profesores y alumnos de la Tecnicatura"); 
+        //Imprimir("Terminamos en unos minutos");
+        ar.com.codesystem.Utileria.imprimir("Ahora si estamos terminando");
+    }
+}
