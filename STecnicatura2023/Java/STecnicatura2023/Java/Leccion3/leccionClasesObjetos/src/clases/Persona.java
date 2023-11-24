6ab27d763b3c8434af8e6b80b8f7637fd16b1ecf
ArriolaBelen <137359966+ArriolaBelen@users.noreply.github.com>
2023-11-24 00:26:53 -0300

    Add files via upload

---
+++ STecnicatura2023/Java/STecnicatura2023/Java/Leccion3/leccionClasesObjetos/src/clases/Persona.java
@@ -0,0 +1,13 @@
+
+package clases;
+
+
+public class Persona{
+    //Atributos de la clase(Característicass)
+    String nombre;
+    String apellido;
+    public void obtenerInformación(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
+}
